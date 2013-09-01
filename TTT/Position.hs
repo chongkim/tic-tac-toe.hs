d686bee4ca7fb9d545c560201f06a9aadb5888b9
Chong Kim <chongkim@yahoo.com>
2013-09-01 07:49:23 -0400

    t8

--- TTT/Position.hs
+++ TTT/Position.hs
@@ -6,68 +6,74 @@ module TTT.Position (
  ,possibleMoves
  ,possiblePositions
  ,isWinFor
+ ,spaces
  ,minimax
  ,bestMove
 )  where
 
 import Data.List
 import Data.List.Split
+import Data.Function
 
 data Position = Position String Char deriving (Show, Eq)
 dim = 3
-size = 3^2
+size = dim^2
 
 initPosition :: Position
 initPosition = Position (replicate size ' ') 'X'
 
 render :: Position -> String
-render (Position board _) =
-  (unlines .
-  intersperse (replicate (dim*4-1) '-') .
-  map (concat . intersperse "|") .
-  chunksOf dim .
+render (Position board turn) = (
+  unlines . intersperse (replicate (dim*4-1) '-') .
+  map (concat . intersperse "|") . chunksOf dim .
   map (\(i,c) -> " "++(if c==' ' then show i else [c])++" ") .
   zip [0..]) board
 
+other :: Char -> Char
+other 'X' = 'O'
+other 'O' = 'X'
+
 move :: Position -> Int -> Position
-move (Position board turn) idx = Position (update idx board) (other turn)
-  where update 0 (_:xs) = turn:xs
-        update idx (x:xs) = x:(update (idx - 1) xs)
-        other 'X' = 'O'
-        other 'O' = 'X'
+move (Position board turn) idx = Position (update idx turn board) (other turn)
+  where update 0 turn (_:xs) = turn:xs
+        update idx turn (x:xs) = x:(update (idx - 1) turn xs)
 
 possibleMoves :: Position -> [Int]
-possibleMoves (Position board _) =
-  (map fst . filter ((==' ') . snd) . zip [0..]) board
+possibleMoves (Position board _) = (
+  map fst . filter (\(i,c) -> c==' ') . zip [0..]) board
 
 possiblePositions :: Position -> [Position]
 possiblePositions position = map (position `move`) (possibleMoves position)
 
 isWinFor :: Position -> Char -> Bool
 isWinFor (Position board _) turn =
-  any lineMatch rows || any lineMatch (transpose rows) ||
-  lineMatch (map (board !!) [0,dim+1..size-1]) ||
-  lineMatch (map (board !!) [dim-1,dim*2-2..size-2])
-  where rows = chunksOf dim board
-        lineMatch = all (==turn)
+  any matchLine rows || any matchLine (transpose rows) ||
+  matchLine (map (board !!) [0,dim+1..size-1]) ||
+  matchLine (map (board !!) [dim-1,dim*2-2..size-2])
+  where matchLine = all (==turn)
+        rows = chunksOf dim board
 
-spaces :: String -> Int
-spaces = length . filter  (==' ')
+spaces :: Position -> Int
+spaces (Position board _) = length (filter (==' ') board)
 
 choose :: Char -> a -> a -> a
-choose 'X' x o = x
-choose 'O' x o = o
+choose 'X' a _ = a
+choose 'O' _ b = b
 
 minimax :: Position -> Int
-minimax position@(Position board turn)
+minimax position@(Position _ turn)
   | position `isWinFor` 'X' = 100
   | position `isWinFor` 'O' = (-100)
-  | spaces board == 0 = 0
+  | spaces position == 0 = 0
   | otherwise =
-      ((choose turn maximum minimum) (map minimax (possiblePositions position)))
-      (choose turn (+) (-))
-      (spaces board)
+    (choose turn add sub)
+    ((choose turn maximum minimum (map minimax (possiblePositions position))))
+    (spaces position)
+  where add a b = a + b + 0
+        sub a b = a - b + 0
 
 -- bestMove :: Position -> Int
--- bestMove position@(Position board turn) =
---   (choose turn maxBy minBy)
+bestMove position = (
+  map (snd ) .
+  map (\idx -> (idx, minimax (position `move` idx))))
+  (possibleMoves position)
--- spec/TTT/PositionSpec.hs
+++ spec/TTT/PositionSpec.hs
@@ -7,90 +7,98 @@ import TTT.Position
 main = hspec $ do
   describe "TTT.Position" $ do
     context "initPosition" $ do
-      it "should create an blank position" $ do
+      it "should create an initial position" $ do
         initPosition `shouldBe` Position ("   "
                                         ++"   "
                                         ++"   ") 'X'
     context "render" $ do
-      it "should render the board" $ do
+      it "should render a position" $ do
         render (Position (" X "
-                        ++"OO "
-                        ++"   ") 'X') `shouldBe` " 0 | X | 2 \n"
+                        ++"XO "
+                        ++"   ") 'O') `shouldBe` " 0 | X | 2 \n"
                                               ++ "-----------\n"
-                                              ++ " O | O | 5 \n"
+                                              ++ " X | O | 5 \n"
                                               ++ "-----------\n"
                                               ++ " 6 | 7 | 8 \n"
     context "move" $ do
       it "should make a move" $ do
-        initPosition `move` 0 `shouldBe` Position "X        " 'O'
+        initPosition `move` 3 `shouldBe`
+          (Position ("   "
+                  ++ "X  "
+                  ++ "   ") 'O')
       it "should make two moves" $ do
-        initPosition `move` 3 `move` 5 `shouldBe`
-          Position ("   "
-                  ++"X O"
-                  ++"   ") 'X'
+        initPosition `move` 3 `move` 0 `shouldBe`
+          (Position ("O  "
+                  ++ "X  "
+                  ++ "   ") 'X')
     context "possibleMoves" $ do
-      it "should find possible moves given a position" $ do
-        possibleMoves (Position ("  X"
-                               ++"OX "
-                               ++"   ") 'O') `shouldBe` [0,1,5,6,7,8]
+      it "should find possible moves for a position" $ do
+        possibleMoves (Position (" X "
+                              ++ "OX "
+                              ++ "   ") 'O') `shouldBe` [0,2,5,6,7,8]
     context "possiblePositions" $ do
-      it "should find possible positions for next move" $ do
-        possiblePositions (Position ("  X"
-                                   ++"OX "
-                                   ++"   ") 'O') `shouldSatisfy`
-          ((Position ("O X"
-                    ++"OX "
-                    ++"   ") 'X') `elem`)
+      it "should find next possible positions from the given position" $ do
+        possiblePositions initPosition `shouldSatisfy`
+          ((Position ("X  "
+                   ++ "   "
+                   ++ "   ") 'O') `elem`)
+        possiblePositions initPosition `shouldSatisfy`
+          ((Position ("   "
+                   ++ " X "
+                   ++ "   ") 'O') `elem`)
     context "isWinFor" $ do
       it "should determine no win" $ do
         initPosition `isWinFor` 'X' `shouldBe` False
-      it "should determine a win for X on first row" $ do
+      it "should determine win for X on first row" $ do
         (Position ("XXX"
                 ++ "   "
                 ++ "   ") 'X') `isWinFor` 'X' `shouldBe` True
-      it "should determine a win for X on second row" $ do
+      it "should determine win for X on second row" $ do
         (Position ("   "
                 ++ "XXX"
                 ++ "   ") 'X') `isWinFor` 'X' `shouldBe` True
-      it "should determine a win for X on third row" $ do
+      it "should determine win for X on third row" $ do
         (Position ("   "
                 ++ "   "
                 ++ "XXX") 'X') `isWinFor` 'X' `shouldBe` True
-      it "should determine a win for O on first col" $ do
+      it "should determine win for O on first col" $ do
         (Position ("O  "
                 ++ "O  "
                 ++ "O  ") 'X') `isWinFor` 'O' `shouldBe` True
-      it "should determine a win for O on second col" $ do
+      it "should determine win for O on second col" $ do
         (Position (" O "
                 ++ " O "
                 ++ " O ") 'X') `isWinFor` 'O' `shouldBe` True
-      it "should determine a win for O on third col" $ do
+      it "should determine win for O on third col" $ do
         (Position ("  O"
                 ++ "  O"
                 ++ "  O") 'X') `isWinFor` 'O' `shouldBe` True
-      it "should determine a win for O on major diagonal" $ do
+      it "should determine win for O on major diagonal" $ do
         (Position ("O  "
                 ++ " O "
                 ++ "  O") 'X') `isWinFor` 'O' `shouldBe` True
-      it "should determine a win for O on minor diagonal" $ do
+      it "should determine win for O on minor diagonal" $ do
         (Position ("  O"
                 ++ " O "
                 ++ "O  ") 'X') `isWinFor` 'O' `shouldBe` True
+    context "spaces" $ do
+      it "should count the spaces in a board" $ do
+        spaces (Position "X        " 'O') `shouldBe` 8
     context "minimax" $ do
       it "should determine a win for X" $ do
         minimax (Position "XXX      " 'O') `shouldBe` 100
       it "should determine a win for O" $ do
-        minimax (Position "OOO      " 'O') `shouldBe` (-100)
+        minimax (Position "OOO      " 'X') `shouldBe` (-100)
       it "should determine a draw" $ do
-        minimax(Position ("XOX"
-                        ++"OXO"
-                        ++"OXO") 'X') `shouldBe` 0
-      it "should determine a win in 1 move for X" $ do
-        minimax (Position "XX       " 'X') `shouldBe` 93
-      it "should determine a win in 1 move for O" $ do
-        minimax (Position "OO       " 'O') `shouldBe` (-93)
-    -- context "bestMove" $ do
-    --   it "should find the best move for win in 1" $ do
-    --     bestMove (Position ("XX "
-    --                       ++"   "
-    --                       ++"   ") 'X') `shouldBe` 2
+        minimax (Position ("OXO"
+                        ++ "XOX"
+                        ++ "XOX") 'X') `shouldBe` 0
+      it "should determine a win for X in 1 move" $ do
+        minimax (Position "XX       " 'X') `shouldBe` 107
+      it "should determine a win for O in 1 move" $ do
+        minimax (Position "OO       " 'O') `shouldBe` (-107)
+    context "bestMove" $ do
+      it "should find the win for X" $ do
+        bestMove (Position ("XX "
+                          ++"   "
+                          ++"   ") 'X') `shouldBe` [] --2
